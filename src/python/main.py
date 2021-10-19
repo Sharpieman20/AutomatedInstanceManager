20f400c7a024fa2e7e43ac65aa3a44b5b6e573ba
Sharpieman20 <>
2021-10-18 21:59:50 -0500

    Fix manual launch terminate early correctly

--- src/python/main.py
+++ src/python/main.py
@@ -111,7 +111,7 @@ def main_loop(sc):
             inst.initialize_after_boot(queues.get_all_instances())
 
     if not settings.should_auto_launch():
-        if len(free_instances) < settings.get_num_instances():
+        if len(settings.get_dead_instances()) > 0:
             schedule_next(sc)
             return
 
