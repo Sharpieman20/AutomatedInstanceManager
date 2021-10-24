1128cc64b36ca7aba1f58616f8db6a2f191390a1
Sharpieman20 <>
2021-10-24 09:48:17 -0500

    Reduce memory for pseudo-instances

--- src/python/launch.py
+++ src/python/launch.py
@@ -10,7 +10,7 @@ def run_cmd(cmd):
     sp.Popen(shlex.split(cmd))
 
 def launch_test_instance(inst):
-    run_cmd('java -Xdock:name=java test/LaunchMockMC.java {}'.format(inst.num))
+    run_cmd('java -Xdock:name=java -Xms64m -Xmx64m test/LaunchMockMC.java {}'.format(inst.num))
 
 def launch_instance(inst):
     if settings.is_test_mode():
