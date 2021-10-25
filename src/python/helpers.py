4f9256c7e2f7215b7e4b0995bd8095f0f1b71e56
Sharpieman20 <>
2021-10-25 18:39:33 -0500

    Use as posix as arg instead of path var

--- src/python/helpers.py
+++ src/python/helpers.py
@@ -47,16 +47,17 @@ def run_ahk(script_name, **kwargs):
     # return ahk.run_script(file_to_script(script_name, **kwargs), blocking=not settings.should_parallelize_ahk())
     ahk_path = _resolve_executable_path()
     script_path = Path.cwd() / "src" / "ahk" / "{}.ahk".format(script_name)
-    args = [ahk_path, "/force", "/ErrorStdOut", script_path.resolve()]
+    print('running with ahk path {}'.format(ahk_path))
+    args = [ahk_path, "/force", "/ErrorStdOut", script_path.resolve().as_posix()]
     for key in kwargs:
         if isinstance(kwargs[key], bool):
             args.append('{}'.format(kwargs[key]).lower())
         else:
             args.append(kwargs[key])
     if settings.should_parallelize_ahk():
-        sp.call(args)
-    else:
         sp.Popen(args)
+    else:
+        sp.call(args)
 
 def add_attempt():
     curr_attempts = 0
