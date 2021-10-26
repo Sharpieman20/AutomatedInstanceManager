5acf0bc71ff55809101cffaf515c04863bd3e9af
Sharpieman20 <>
2021-10-25 20:15:32 -0500

    Target jdk process

--- src/python/helpers.py
+++ src/python/helpers.py
@@ -23,9 +23,10 @@ def get_pids():
     all_pids = []
     print('get all pids')
     for process in wmi.WMI().Win32_Process():
-        print('name {} pid {} caption {} commandline {}'.format(process.Name, process.ProcessId, process.Caption, process.Commandline))
+        # print('name {} pid {} caption {} commandline {}'.format(process.Name, process.ProcessId, process.Caption, process.Commandline))
         if 'java' in process.Name.lower():
-            all_pids.append(process.ProcessId)
+            if 'jdk' in process.Commandline:
+                all_pids.append(process.ProcessId)
     print('-------')
     return all_pids
 
