3608c9830100265b3d4de18251f9daa9192b8b42
Sharpieman20 <>
2021-11-04 19:05:37 -0500

    Fix setup.py error (not having requests installed + accessing src/requirements.txt before it was downloaded

--- dist/setup.py
+++ dist/setup.py
@@ -12,25 +12,9 @@ def run_cmd(cmd):
 run_cmd('py -m ensurepip')
 run_cmd('py -m pip install --upgrade setuptools')
 run_cmd('py -m pip install wheel')
-
-wheel_urls = []
-
-wheel_urls.append('https://github.com/Sharpieman20/AutomatedInstanceManager/raw/main/res/six-1.16.0-py2.py3-none-any.whl')
-wheel_urls.append('https://github.com/Sharpieman20/AutomatedInstanceManager/raw/main/res/websocket_client-1.2.1-py2.py3-none-any.whl')
-wheel_urls.append('https://github.com/Sharpieman20/AutomatedInstanceManager/raw/main/res/obs_websocket_py-0.5.3-py3-none-any.whl')
-# wheel_urls.append('https://github.com/Sharpieman20/MultiResetTinder/raw/main/res/psutil-5.8.0-cp39-cp39-win_amd64.whl')
-
-for url in wheel_urls:
-    run_cmd('py -m pip install {}'.format(url))
-# run_cmd('py -m pip install res/')
-run_cmd('py -m pip install --only-binary :all: -r src/requirements.txt'.format(os.path.dirname(sys.executable)))
+run_cmd('py -m pip install --only-binary wheel requests')
 
 import requests
-
-defaults_dir = Path.cwd() / 'defaults'
-
-if defaults_dir.exists():
-    shutil.rmtree(Path.cwd() / 'defaults')
 
 src_dir = Path.cwd() / 'src'
 if src_dir.exists():
@@ -51,9 +35,25 @@ with zipfile.ZipFile('release.zip', 'r') as zip_ref:
 
 os.remove('release.zip')
 
+wheel_urls = []
+
+wheel_urls.append('https://github.com/Sharpieman20/AutomatedInstanceManager/raw/main/res/six-1.16.0-py2.py3-none-any.whl')
+wheel_urls.append('https://github.com/Sharpieman20/AutomatedInstanceManager/raw/main/res/websocket_client-1.2.1-py2.py3-none-any.whl')
+wheel_urls.append('https://github.com/Sharpieman20/AutomatedInstanceManager/raw/main/res/obs_websocket_py-0.5.3-py3-none-any.whl')
+# wheel_urls.append('https://github.com/Sharpieman20/MultiResetTinder/raw/main/res/psutil-5.8.0-cp39-cp39-win_amd64.whl')
+
+for url in wheel_urls:
+    run_cmd('py -m pip install {}'.format(url))
+# run_cmd('py -m pip install res/')
+run_cmd('py -m pip install --only-binary :all: -r src/requirements.txt'.format(os.path.dirname(sys.executable)))
+
+defaults_dir = Path.cwd() / 'defaults'
+
+if defaults_dir.exists():
+    shutil.rmtree(Path.cwd() / 'defaults')
+
 settings_url = 'https://raw.githubusercontent.com/Sharpieman20/AutomatedInstanceManager/main/settings.json'
 r = requests.get(settings_url, allow_redirects=True)
-
 
 defaults_dir.mkdir()
 settings_json = defaults_dir / 'settings.json'
