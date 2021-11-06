4f47eee784bb4e3141d3a3e5eed054a8fd0acaad
Sharpieman20 <>
2021-11-05 21:56:47 -0500

    Fix syntax error

--- src/python/obs.py
+++ src/python/obs.py
@@ -97,7 +97,7 @@ def set_new_primary(inst):
             primary_instance.mark_hidden()
             primary_pid = primary_instance.pid
         if settings.use_switching_daemon():
-            with open(hlp.get_pipe_file(), 'w') as fil:
+            with open(hlp.get_pipe_file_location(), 'w') as fil:
                 fil.write('{}\n{}'.format(primary_pid, inst.pid))
         if inst.is_focused():
             global focused_instance
