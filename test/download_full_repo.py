fde6219c5704b6def4afedb1b54cc141d9d6e3ba
Sharpieman20 <>
2022-01-15 16:17:28 -0600

    Fix download_full_repo again

--- test/download_full_repo.py
+++ test/download_full_repo.py
@@ -2,19 +2,11 @@
 from pathlib import Path
 import subprocess as sp
 import sys, os, shlex, urllib
-import urllib.request
+import requests
 
 os.chdir(Path(__file__).parent.resolve())
 
-fullrepo_file = Path.cwd().parent / 'fullrepo.zip'
-
-if fullrepo_file.exists():
-    fullrepo_file.unlink()
-
 fullrepo_url = 'https://raw.githubusercontent.com/Sharpieman20/AutomatedInstanceManager/rewrite/build/fullrepo.zip'
 
-fullrepo_resource = urllib.request.urlopen(fullrepo_url)
-
-fullrepo_content = fullrepo_resource.read().decode(fullrepo_resource.headers.get_content_charset())
-
-open('fullrepo.zip', 'w').write(fullrepo_content)
+r = requests.get(fullrepo_url, allow_redirects=True)
+open('../fullrepo.zip', 'wb').write(r.content)
