02bbcee7069a4cb6739c6f9c485a9ee2eab5ef7e
Sharpieman20 <>
2022-01-15 16:15:33 -0600

    Fix download_full_repo.py

--- test/download_full_repo.py
+++ test/download_full_repo.py
@@ -6,10 +6,10 @@ import urllib.request
 
 os.chdir(Path(__file__).parent.resolve())
 
-setup_py_file = Path.cwd() / 'setup.py'
+fullrepo_file = Path.cwd().parent / 'fullrepo.zip'
 
-if setup_py_file.exists():
-    setup_py_file.unlink()
+if fullrepo_file.exists():
+    fullrepo_file.unlink()
 
 fullrepo_url = 'https://raw.githubusercontent.com/Sharpieman20/AutomatedInstanceManager/rewrite/build/fullrepo.zip'
 
@@ -17,4 +17,4 @@ fullrepo_resource = urllib.request.urlopen(fullrepo_url)
 
 fullrepo_content = fullrepo_resource.read().decode(fullrepo_resource.headers.get_content_charset())
 
-open('fullrepo.py', 'w').write(fullrepo_content)
+open('fullrepo.zip', 'w').write(fullrepo_content)
