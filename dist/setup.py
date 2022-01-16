3ad737248369fb3a7e833a1e9bab291297a39f10
Sharpieman20 <>
2022-01-15 21:00:15 -0600

    Switch to using global-hotkeys wheel

--- dist/setup.py
+++ dist/setup.py
@@ -40,6 +40,7 @@ wheel_urls = []
 wheel_urls.append('https://github.com/Sharpieman20/AutomatedInstanceManager/raw/rewrite/res/six-1.16.0-py2.py3-none-any.whl')
 wheel_urls.append('https://github.com/Sharpieman20/AutomatedInstanceManager/raw/rewrite/res/websocket_client-1.2.1-py2.py3-none-any.whl')
 wheel_urls.append('https://github.com/Sharpieman20/AutomatedInstanceManager/raw/rewrite/res/obs_websocket_py-0.5.3-py3-none-any.whl')
+wheel_urls.append('https://github.com/Sharpieman20/AutomatedInstanceManager/raw/rewrite/res/global_hotkeys-0.0.4-py3-none-any.whl')
 # wheel_urls.append('https://github.com/Sharpieman20/MultiResetTinder/raw/main/res/psutil-5.8.0-cp39-cp39-win_amd64.whl')
 
 for url in wheel_urls:
